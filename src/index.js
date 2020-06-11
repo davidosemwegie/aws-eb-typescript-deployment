367b320f11e96bd27715ec09998d979999bebc8f
David Osemwegie <dosemwegie@gmail.com>
2020-06-10 20:44:41 -0600

    added another route

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ dotenv.config()
 
 const app = express()
 const port = process.env.port || 3000
-app.get("/", (req, res) => res.send("Hello world"))
+app.get("/", (req, res) => res.send(`Welcome to the ${process.env.name} api`))
+app.get("/hello", (req, res) => res.send("Hello World"))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
