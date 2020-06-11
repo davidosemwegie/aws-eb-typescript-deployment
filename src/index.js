f4374da512b7dc9f4780cad9c32226d374bbce55
David Osemwegie <dosemwegie@gmail.com>
2020-06-10 20:10:37 -0600

    removed env variable

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ dotenv.config()
 
 const app = express()
 const port = process.env.port || 3000
-app.get("/", (req, res) => res.send(process.env.NAME))
+app.get("/", (req, res) => res.send("Hello world"))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
