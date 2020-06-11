48afc7802d1cfabca8b82f87fb4b679cf9cf17da
David Osemwegie <dosemwegie@gmail.com>
2020-06-10 19:56:00 -0600

    updated port

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const app = express()
-const port = 3000
+const port = process.env.port || 3000
 app.get("/", (req, res) => res.send(process.env.NAME))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
