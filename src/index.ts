4463082bf72d2af8a05a16618de2d71efe20dc34
David Osemwegie <dosemwegie@gmail.com>
2020-06-11 02:02:52 -0600

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from "express"
-import dotenv from "dotenv"
-
-dotenv.config()
 
 const app = express()
-const port = process.env.port || 3000
-app.get("/", (req, res) => res.send(`Welcome to the ${process.env.name} api`))
-app.get("/hello", (req, res) => res.send("Hello World"))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.use((req, res) => {
+  res.send("Tracflo API Test 7")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log("server is ready.")
+})
