b9295cdc619304977430b9ae8106f2ef1975afdc
David Osemwegie <dosemwegie@gmail.com>
2020-06-11 02:17:52 -0600

    minor updates

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from "express"
 
 const app = express()
+const port = process.env.PORT || 3000
 
 app.use((req, res) => {
   res.send("Tracflo API Test 7")
 })
 
+app.get("/hello", (req, res) => {
+  res.send("Hello world")
+})
+
 app.listen(process.env.PORT, () => {
-  console.log("server is ready.")
+  console.log(`server is up and running on port ${port}`)
 })
