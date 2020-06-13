710c6086bdbfffa87874c710bd6e5d94b923aade
David Osemwegie <dosemwegie@gmail.com>
2020-06-12 23:41:13 -0600

    commit

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@ const port = process.env.PORT || 8080
 app.get("/", (req, res) => {
   res.send("Tracflo API Test v2")
 })
-app.get("/final", (req, res) => {
-  res.send("This is interesting, & okay this is actually the final test")
+app.get("/minor", (req, res) => {
+  res.send("<i>")
 })
 
 app.get("/env", (req, res) => {
